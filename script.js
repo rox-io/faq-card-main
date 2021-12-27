73afef34daf94852a52554fd561dd1b5c418cfad
rox-io <rox_prawl@icloud.com>
2021-12-27 15:53:40 -0500

    updated click event listener

--- script.js
+++ script.js
@@ -3,55 +3,39 @@ const hiddenLis = document.querySelectorAll("ul .hidden-li");
 const firstLi = document.querySelectorAll("ul .first-li");
 const uls = document.querySelectorAll("#accordion-list ul");
 
-let arrowUp = false;
-for(let i = 0; i < uls.length; i++) {
-    uls[i].addEventListener("click", () => {
-        console.log('clicked');
-        if(!arrowUp) {
-            activeArrow(arrows, arrows[i]);
-        }
-        else {
-            arrows[i].classList.remove("rotate-arrows");
-            arrowUp = false;
-        }
-
-    //uls[i].addEventListener("click", () => {
-    //    if(!arrowUp) {
-    //        activeArrow(arrows, arrows[i]);
-    //        hideLi(hiddenLis, hiddenLis[i]);
-    //        removeFontStyle(firstLi, firstLi[i]);
-    //        arrowUp = true;
-    //        console.log(arrowUp);
-    //    }
-    //    else {
-    //        hideLi(hiddenLis, hiddenLis[i]);
-    //        removeFontStyle(firstLi, firstLi[i]);
-    //        arrowUp = false;
-    //        console.log(arrowUp);
-    //    }
-    //})
-    })
+let arrClicked;
+for (let i = 0; i < uls.length; i++) {
+  uls[i].addEventListener("click", () => {
+    if(arrClicked === i) {
+      console.log("arrow toggle");
+      arrows[i].classList.toggle("rotate-arrows");
+    }
+    else {
+      activeArrow(arrows, arrows[i]);
+      arrClicked = i;
+      console.log(`${arrClicked} another clicked arrow`);
+    }
+  })
 }
 
 //check active list
 function activeArrow(arrows, selected) {
-     for(let arrow of arrows) {
-        arrow.classList.remove("rotate-arrows");
-    }
-    selected.classList.add("rotate-arrows");
-    arrowUp = true;
+  for (let arrow of arrows) {
+    arrow.classList.remove("rotate-arrows");
+  }
+  selected.classList.add("rotate-arrows");
 }
 
 function hideLi(lis, selected) {
-    for(let li of lis) {
-        li.classList.remove("show-lis");
-    }
-    selected.classList.add("show-lis");
+  for (let li of lis) {
+    li.classList.remove("show-lis");
+  }
+  selected.classList.add("show-lis");
 }
 
 function removeFontStyle(lis, selected) {
-    for(let li of lis) {
-        li.classList.remove("bold-font");
-    }
-    selected.classList.add("bold-font");
+  for (let li of lis) {
+    li.classList.remove("bold-font");
+  }
+  selected.classList.add("bold-font");
 }
