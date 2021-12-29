f5597b6cd9bce7ede0759f54e766d64654eec2a1
superduperocks <rox_prawl@icloud.com>
2021-12-28 19:34:15 -0500

    updated click event listener

--- script.js
+++ script.js
@@ -1,24 +1,28 @@
 const arrows = document.querySelectorAll("ul img");
 const hiddenLis = document.querySelectorAll("ul .hidden-li");
-const firstLi = document.querySelectorAll("ul .first-li");
+const firstLis = document.querySelectorAll("ul .first-li");
 const uls = document.querySelectorAll("#accordion-list ul");
 
-let arrClicked;
+let currentSelectedList;
+let previousSelectedList;
 for (let i = 0; i < uls.length; i++) {
   uls[i].addEventListener("click", () => {
-    if(arrClicked === i) {
-      console.log("arrow toggle");
+      previousSelectedList = currentSelectedList;
+      currentSelectedList = i;
+    if(currentSelectedList === previousSelectedList) {
       arrows[i].classList.toggle("rotate-arrows");
+      hiddenLis[i].classList.toggle("show-lis");
+      firstLis[i].classList.toggle("bold-font");
     }
     else {
       activeArrow(arrows, arrows[i]);
-      arrClicked = i;
-      console.log(`${arrClicked} another clicked arrow`);
+      hideLi(hiddenLis, hiddenLis[i]);
+      removeFontStyle(firstLis, firstLis[i]);
     }
   })
 }
 
-//check active list
+//ARROW ROTATION
 function activeArrow(arrows, selected) {
   for (let arrow of arrows) {
     arrow.classList.remove("rotate-arrows");
@@ -26,6 +30,7 @@ function activeArrow(arrows, selected) {
   selected.classList.add("rotate-arrows");
 }
 
+//DISPLAY LISTS
 function hideLi(lis, selected) {
   for (let li of lis) {
     li.classList.remove("show-lis");
@@ -33,6 +38,7 @@ function hideLi(lis, selected) {
   selected.classList.add("show-lis");
 }
 
+//BOLD FONT FOR SELECTED LIST
 function removeFontStyle(lis, selected) {
   for (let li of lis) {
     li.classList.remove("bold-font");
